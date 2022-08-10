8868110c278c1d76deae02105af68178d1e76961
Nastia Kiseleowa <nastia.kiseleowa@yandex.ru>
2022-08-10 22:29:45 +0300

    add task 15

---
+++ task15/Program.cs
@@ -0,0 +1,9 @@
+﻿while (true)
+{
+    Console.Write("Введите день недели: ");
+    int day = Convert.ToInt32(Console.ReadLine());
+    if (day < 6)
+        Console.WriteLine("нет");
+    else if (day == 6 | day == 7)
+        Console.WriteLine("да");
+}
