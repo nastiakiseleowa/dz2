77eb3ca44df748fb6e5b6f1bbd28f6ea6d17cd3f
Nastia Kiseleowa <nastia.kiseleowa@yandex.ru>
2022-08-16 12:30:43 +0300

    add task 10

---
+++ task10/Program.cs
@@ -0,0 +1,6 @@
+﻿while (true)
+{
+    Console.Write("Введите трехзначное число: ");
+    string Number = Convert.ToString(Convert.ToInt32(Console.ReadLine()));
+    Console.WriteLine("Вторая цифра: " + Number[1]);
+}
