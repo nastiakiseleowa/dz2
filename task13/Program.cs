26d944a4bb9cf157db6b14a7b260055bba850698
Nastia Kiseleowa <nastia.kiseleowa@yandex.ru>
2022-08-16 12:30:29 +0300

    add task 13

---
+++ task13/Program.cs
@@ -0,0 +1,13 @@
+﻿while (true)
+{
+    Console.Write("Введи число: ");
+    string Number = Convert.ToString(Convert.ToInt32(Console.ReadLine()));
+    if (Number.Length > 2)
+    {
+        Console.WriteLine("Третья цифра: " + Number[2]);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+}
